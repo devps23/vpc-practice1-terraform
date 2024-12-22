32b1bd9b14b5a8fc890eb6d376729ffa0c2c08ad
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-12-22 18:57:26 +0530

    comment a code for a while

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ module "app" {
   vpc_id = module.vpc.vpc_id
   subnets = module.vpc.frontend_subnets
 }
-module "vpc"{
+module "vpc1"{
   source = "./modules/vpc"
   vpc_id = var.vpc_id
   env = var.env
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -6,7 +6,7 @@ resource "aws_vpc" "vpc" {
   }
 }
 # create public  subnets
-resource "aws_subnet" "public" {
+resource "aws_subnet" "public_subnets" {
   count      = length(var.public_subnets)
   vpc_id     = aws_vpc.vpc.id
   cidr_block = var.public_subnets[count.index]
@@ -17,33 +17,13 @@ resource "aws_subnet" "public" {
   }
 }
 # create frontend subnets
-resource "aws_subnet" "frontend" {
+resource "aws_subnet" "frontend_subnets" {
   count      = length(var.frontend_subnets)
   vpc_id     = aws_vpc.vpc.id
   cidr_block = var.frontend_subnets[count.index]
   availability_zone = var.availability_zones[count.index]
   tags = {
     Name = "${var.env}-frontend-${count.index+1}"
-  }
-}
-# create backend subnets
-resource "aws_subnet" "backend" {
-  count      = length(var.backend_subnets)
-  vpc_id     = aws_vpc.vpc.id
-  cidr_block = var.backend_subnets[count.index]
-  availability_zone = var.availability_zones[count.index]
-  tags = {
-    Name = "${var.env}-backend-${count.index+1}"
-  }
-}
-# create db subnets
-resource "aws_subnet" "db" {
-  count      = length(var.db_subnets)
-  vpc_id     = aws_vpc.vpc.id
-  cidr_block = var.db_subnets[count.index]
-  availability_zone = var.availability_zones[count.index]
-  tags = {
-    Name = "${var.env}-db-${count.index+1}"
   }
 }
 # peer connection between two vpc id's
@@ -63,7 +43,11 @@ resource "aws_route_table" "frontend_route_table" {
     cidr_block = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat_gateway[count.index].id
   }
-   tags = {
+  route{
+    cidr_block = var.default_cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  }
+  tags = {
     Name = "${var.env}-frontend-rt-tbl-${count.index+1}"
   }
 }
@@ -71,10 +55,10 @@ resource "aws_route_table" "frontend_route_table" {
 resource "aws_route_table" "public_route_table" {
   count = length(var.public_subnets)
   vpc_id = aws_vpc.vpc.id
-route{
-  cidr_block = var.default_cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-}
+  route{
+    cidr_block = var.default_cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  }
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
@@ -84,6 +68,70 @@ route{
     Name = "${var.env}-public-rt-tbl-${count.index+1}"
   }
 }
+
+# create public route table association with backend subnet
+resource "aws_route_table_association" "public-tbl-ass" {
+  count = length(var.public_subnets)
+  subnet_id      = aws_subnet.public_subnets[count.index].id
+  route_table_id = aws_route_table.public_route_table[count.index].id
+}
+# by default there is an association between subnets and route table id
+#so when we create a custom route table then we have to associate for custom subnet and with route table id
+resource "aws_route_table_association" "frontend-tbl-ass" {
+  count = length(var.frontend_subnets)
+  subnet_id      = aws_subnet.frontend_subnets[count.index].id
+  route_table_id = aws_route_table.frontend_route_table[count.index].id
+}
+# create an internet gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "${var.env}-igw"
+  }
+}
+resource "aws_nat_gateway" "nat_gateway" {
+  count = length(var.public_subnets)
+  allocation_id = aws_eip.eip[count.index].id
+  subnet_id     = aws_subnet.public_subnets[count.index].id
+
+  tags = {
+    Name = "${var.env}-nat"
+  }
+}
+#  create a eip
+resource "aws_eip" "eip" {
+  count = length(var.public_subnets)
+  domain   = "vpc"
+}
+# resource "aws_route" "frontend_route" {
+#   count                     = length(var.frontend_subnets)
+#   route_table_id            = aws_route_table.frontend_route_table[count.index].id
+#   destination_cidr_block    = var.default_cidr_block
+#   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+# }
+# create a nat gateway
+
+# # create backend subnets
+# resource "aws_subnet" "backend" {
+#   count      = length(var.backend_subnets)
+#   vpc_id     = aws_vpc.vpc.id
+#   cidr_block = var.backend_subnets[count.index]
+#   availability_zone = var.availability_zones[count.index]
+#   tags = {
+#     Name = "${var.env}-backend-${count.index+1}"
+#   }
+# }
+# # create db subnets
+# resource "aws_subnet" "db" {
+#   count      = length(var.db_subnets)
+#   vpc_id     = aws_vpc.vpc.id
+#   cidr_block = var.db_subnets[count.index]
+#   availability_zone = var.availability_zones[count.index]
+#   tags = {
+#     Name = "${var.env}-db-${count.index+1}"
+#   }
+# }
+
 # create a backend route table
 # resource "aws_route_table" "backend_route_table" {
 #   count = length(var.backend_subnets)
@@ -108,19 +156,6 @@ route{
 #     Name = "${var.env}-db-rt-tbl-${count.index+1}"
 #   }
 # }
-# create public route table association with backend subnet
-resource "aws_route_table_association" "public-tbl-ass" {
-  count = length(var.public_subnets)
-  subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public_route_table[count.index].id
-}
-# by default there is an association between subnets and route table id
-#so when we create a custom route table then we have to associate for custom subnet and with route table id
-resource "aws_route_table_association" "frontend-tbl-ass" {
-  count = length(var.frontend_subnets)
-  subnet_id      = aws_subnet.frontend[count.index].id
-  route_table_id = aws_route_table.frontend_route_table[count.index].id
-}
 # create backend route table association with backend subnet
 # resource "aws_route_table_association" "backend-tbl-ass" {
 #   count = length(var.backend_subnets)
@@ -133,19 +168,7 @@ resource "aws_route_table_association" "frontend-tbl-ass" {
 #   subnet_id      = aws_subnet.db[count.index].id
 #   route_table_id = aws_route_table.db_route_table[count.index].id
 # }
-# create an internet gateway
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.vpc.id
-  tags = {
-    Name = "${var.env}-igw"
-  }
-}
-resource "aws_route" "frontend_route" {
-  count                     = length(var.frontend_subnets)
-  route_table_id            = aws_route_table.frontend_route_table[count.index].id
-  destination_cidr_block    = var.default_cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-}
+
 # resource "aws_route" "backend_route" {
 #   count                     = length(var.backend_subnets)
 #   route_table_id            = aws_route_table.backend_route_table[count.index].id
@@ -164,21 +187,7 @@ resource "aws_route" "frontend_route" {
 #   destination_cidr_block    = var.default_cidr_block
 #   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 # }
-# create a nat gateway
-resource "aws_nat_gateway" "nat_gateway" {
-  count = length(var.public_subnets)
-  allocation_id = aws_eip.eip[count.index].id
-  subnet_id     = aws_subnet.public[count.index].id
 
-  tags = {
-    Name = "${var.env}-nat"
-  }
- }
-#  create a eip
-resource "aws_eip" "eip" {
-  count = length(var.public_subnets)
-    domain   = "vpc"
-  }
 
 
 # default route table id =rtb-00125ce6494f06f9b
