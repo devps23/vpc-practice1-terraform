230bb9a6c60fe2425f8ba1a21b2ca3a9202e09ae
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-12-22 19:02:14 +0530

    comment a code for a while

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -2,11 +2,14 @@ output "vpc_id" {
   value = aws_vpc.vpc.id
 }
 output "frontend_subnets" {
-  value = aws_subnet.frontend.*.id
+  value = aws_subnet.frontend_subnets.*.id
 }
-output "backend_subnets" {
-  value = aws_subnet.backend.*.id
+output "public_subnets" {
+  value = aws_subnet.public_subnets.*.id
 }
-output "db_subnets" {
-  value = aws_subnet.db.*.id
-}
+# output "backend_subnets" {
+#   value = aws_subnet.backend.*.id
+# }
+# output "db_subnets" {
+#   value = aws_subnet.db.*.id
+# }
