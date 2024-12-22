410c6ce9404115fc117ce29ebd8022f143f81ba6
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-12-22 18:59:38 +0530

    comment a code for a while

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ module "app" {
   vpc_id = module.vpc.vpc_id
   subnets = module.vpc.frontend_subnets
 }
-module "vpc1"{
+module "vpc"{
   source = "./modules/vpc"
   vpc_id = var.vpc_id
   env = var.env
