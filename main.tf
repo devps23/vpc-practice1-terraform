8e86b96393c934aee1dda18d4103b772bc47a8df
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-12-22 15:15:31 +0530

    association between subnets with route table id

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
-# module "app" {
-#   source = "./modules/app"
-#   instance_type="t2.micro"
-#   env = var.env
-#   subnets = var.frontend_subnets
-#   vpc_id = var.vpc_id
-# }
-module "vpc"{
-  source = "./modules/vpc"
-  vpc_id = var.vpc_id
+module "app" {
+  source = "./modules/app"
+  instance_type="t2.micro"
   env = var.env
-  availability_zones = var.availability_zones
-  frontend_subnets = var.frontend_subnets
-  backend_subnets = var.backend_subnets
-  db_subnets = var.db_subnets
-  default_vpc_id=var.default_vpc_id
+  subnets = var.frontend_subnets
+  vpc_id = var.vpc_id
 }
+# module "vpc"{
+#   source = "./modules/vpc"
+#   vpc_id = var.vpc_id
+#   env = var.env
+#   availability_zones = var.availability_zones
+#   frontend_subnets = var.frontend_subnets
+#   backend_subnets = var.backend_subnets
+#   db_subnets = var.db_subnets
+#   default_vpc_id=var.default_vpc_id
+# }
