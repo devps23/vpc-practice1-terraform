5cbd463a49d3eaac1a7db69fbc1454f0d8d990e2
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-12-22 17:14:55 +0530

    create a security group add security group to the respective instance

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "instance" {
   ami = data.aws_ami.ami.image_id
   instance_type = var.instance_type
   subnet_id = var.subnets[0]
+  vpc_security_group_ids = [aws_security_group.security_group.id]
     instance_market_options {
     market_type = "spot"
     spot_options {
@@ -12,5 +13,27 @@ resource "aws_instance" "instance" {
   }
   tags = {
     Name = "${var.env}-demo"
+  }
+}
+# create a security group
+resource "aws_security_group" "security_group" {
+  name        = "${var.env}-sg"
+  vpc_id      = var.vpc_id
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+  tags = {
+    Name = "${var.env}-sg"
   }
 }
