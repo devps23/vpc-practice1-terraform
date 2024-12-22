3d64cd70c465a371b766691ae8529209363b7f44
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-12-22 17:58:12 +0530

    eip

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -71,7 +71,10 @@ resource "aws_route_table" "frontend_route_table" {
 resource "aws_route_table" "public_route_table" {
   count = length(var.public_subnets)
   vpc_id = aws_vpc.vpc.id
-
+route{
+  cidr_block = var.default_cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
